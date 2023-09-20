c4d8580d677f56ce74618c408d896cc7a3fca775
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-09-20 11:35:41 +0200

    Question 2.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import Image from 'next/image'
+import {BreadCrumbs, Button, SectionContainer} from "tp-kit/components";
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import {ProductCardLayout, ProductGridLayout} from "tp-kit/components/products";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main>
+        <BreadCrumbs
+            items={[
+              {
+                label: 'Accueil',
+                url: '#'
+              }
+            ]}
+        />
+            {categories.map(category =>
+                <>
+                <h1><b>{category.name} ({category.products.length})</b></h1>
+                <ProductGridLayout products={category.products}>
+                    {product => <ProductCardLayout
+                        button={<Button variant="ghost" fullWidth="true">Ajouter au panier</Button>}
+                        product={ product }
+                    />
+                    }
+                </ProductGridLayout>
+                </>
+                  )}
     </main>
   )
 }
