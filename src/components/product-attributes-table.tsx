6f0b9f92bd71f646da4d49bf2df551eb85aa40c1
tomparis1 <paristom7@gmail.com>
2023-09-26 13:17:35 +0200

    Question 3.4

---
+++ src/components/product-attributes-table.tsx
@@ -0,0 +1,40 @@
+import {ProductAttribute} from "@/types"
+
+import {ProductRating} from "tp-kit/components"
+
+import styles from '../app/product-attributes-table.module.css'
+
+
+type Props = {
+
+    attributes: ProductAttribute[]
+
+}
+
+
+export default function ProductAttributeTable({attributes}: Props) {
+
+    return (
+
+        <table className={styles.table}>
+
+            <tbody>
+
+            {attributes.map((attribute, index) => <> {index == 0 ? "" : <hr/>}
+                <tr>
+
+                    <td>{attribute.label}</td>
+
+                    <td><ProductRating icon="circle" value={attribute.rating}/></td>
+
+                </tr>
+            </>)}
+
+            </tbody>
+
+
+        </table>
+
+    )
+
+}
